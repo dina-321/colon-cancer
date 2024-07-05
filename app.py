e7fe6944c436b8a129219ddabbfada8b60c35c66
dina-321 <162456097+dina-321@users.noreply.github.com>
2024-07-06 02:27:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,47 @@
+
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 import numpy as np
-import os
 
 app = Flask(__name__)
 
-# Load the model
-model = load_model('my_weights.h5')
+# Load the trained model
+model = load_model('my_weights_new_adam.h5')
 
-def model_predict(image_path, model):
+# Define a function to preprocess the image
+def preprocess_image(image_path):
     image = load_img(image_path, target_size=(64, 64))
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
-    result = model.predict(image)
-    return result
+    return image
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-    if file:
-        file_path = os.path.join('uploads', file.filename)
-        file.save(file_path)
-        result = model_predict(file_path, model)
-        os.remove(file_path)  # Remove the file after prediction
-        if np.argmax(result) == 0:
-            prediction = "Normal"
-        else:
-            prediction = "Cancer"
-        return jsonify({'prediction': prediction})
+    if 'files' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    files = request.files.getlist('files')
+    if len(files) == 0:
+        return jsonify({'error': 'No selected files'})
+
+    predictions = {}
+    for file in files:
+        if file:
+            file_path = f"./{file.filename}"
+            file.save(file_path)
+
+            image = preprocess_image(file_path)
+            result = model.predict(image)
+
+            if result[0][0] > 0.5:
+                prediction = "normal"
+            else:
+                prediction = "cancer"
+
+            predictions[file.filename] = prediction
+
+    return jsonify(predictions)
 
 if __name__ == '__main__':
-    if not os.path.exists('uploads'):
-        os.makedirs('uploads')
     app.run(debug=True)
